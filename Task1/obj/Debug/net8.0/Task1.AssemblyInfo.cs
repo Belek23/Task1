ce607faaac1780250ed594988b88424470971941
Belek Abdy <beshik2013@yandex.ru>
2024-02-02 17:32:19 +0300

    add lesson:task2

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+876f693040302afb24a14996b833478cc9674970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8612711928c2c730bf55e5bfd05f60ee242eafc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
